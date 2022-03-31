47d4f6117fd4a9198130438ca197770fdcbb2a5d
kim jin seong <wlsdiqkdrk@naver.com>
2022-03-31 17:54:38 +0900

    aws

---
+++ .next/static/pCsT5rspB12_OQil71im5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-207ea16e6c1295be6959.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d9a2c35f5e445aae2c32.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
